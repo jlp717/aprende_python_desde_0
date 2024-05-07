22ecc6b4adf37bcb097b768f4bd50022b264cab8
jlacpel <90418425+jlp717@users.noreply.github.com>
2024-05-07 20:07:57 +0200

    Create cap1.py (teoria)

---
+++ CAP1_INTRODUCCIÓN_PYTHON/cap1.py
@@ -0,0 +1,38 @@
+# Tipos de Datos Básicos
+
+#     Enteros: int
+#     Flotantes: float
+#     Cadenas: str
+#     Booleanos: bool
+
+#Variables
+nombre = "Ana"
+edad = 25
+
+#Operadores aritméricos (suma y resta)
+suma = 3 + 5 #8
+resta = 10 - 2 #8
+
+#Operadores relacionales(igual, mayor, menor)
+es_igual = (5 == 5)
+mayor = (10 > 2)
+menor = (2 < 5)
+
+print(es_igual) #True
+print(mayor) #True
+print(menor) #True
+
+#Operadores lógicos
+y_logico = (True and False)
+o_logico = (True or False)
+
+print(y_logico) #False
+print(o_logico) #True
+
+# Esto es un comentario
+
+#Entrada estándar usando input
+nombre = input("¿Cómo te llamas? ")
+
+#Salida estándar usando input
+print(f"Hola, {nombre}")
