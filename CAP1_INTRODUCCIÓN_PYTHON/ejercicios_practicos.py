f7279a2c11dfabab1cecdc7e11c9cdd070662f48
jlacpel <90418425+jlp717@users.noreply.github.com>
2024-05-07 20:08:26 +0200

    Enunciados de los ejercicios propuestos cap1

---
+++ CAP1_INTRODUCCIÓN_PYTHON/ejercicios_practicos.py
@@ -0,0 +1,7 @@
+#Ejercicio 1: Crear un programa que pida al usuario su nombre y edad, y lo salude.
+
+#Ejercicio 2: Escribir un programa que calcule el área de un triángulo dado su base y altura.
+#Pista: La altura del triángulo es ((base * altura) / 2)
+
+#Ejercicio 3: Realizar un programa que determine si un número ingresado es par o impar.
+#Pista: Con % sabemos el resto de una divisón
